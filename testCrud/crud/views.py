e4007e3e0ac0fe3f51e139631af96a19706c10a6
ToniToledo <toledo270366@gmail.com>
2025-02-20 08:50:13 +0000

    Guardar facturas en BBDD y en la web

--- testCrud/crud/views.py
+++ testCrud/crud/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Invoice
 from .forms import InvoiceForm
 
 def invoice_list_and_create(request):
-    form = InvoiceForm
+
+    if request.method == "POST":
+        form = InvoiceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('crud:crud_list')
+    else:
+        form = InvoiceForm()
+
     invoices = Invoice.objects.all()
 
     return render(request, 'invoice_list.html', {
