06511975e6905aa636962dd96582bdf69cc507f0
ToniToledo <toledo270366@gmail.com>
2025-06-18 13:11:33 +0000

    [ADD] sidebar y navbar

--- simpleERP/manage.py
+++ simpleERP/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testCrud.settings_tt')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simpleERP.settings_tt')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- simpleERP/simpleERP/asgi.py
+++ simpleERP/simpleERP/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for testCrud project.
+ASGI config for simpleERP project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testCrud.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simpleERP.settings')
 
 application = get_asgi_application()
